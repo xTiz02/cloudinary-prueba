c9153c48b11705b94722c29537983867667e7ef0
Piero Alessandro Rodriguez Diaz <u21231696@utp.edu.pe>
2024-05-24 12:13:36 -0500

    Subir imágenes a cloudinarity

---
+++ src/test/java/org/prd/uploadfilescloud/UploadFilesCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prd.uploadfilescloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadFilesCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
